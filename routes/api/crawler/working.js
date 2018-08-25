765c41166be74ff012a9e7daf664b4118dc641fb
Micalgenus <micalgenus@gmail.com>
2018-08-25 23:33:39 +0900

    Remove all working

--- routes/api/crawler/working.js
+++ routes/api/crawler/working.js
@@ -6,6 +6,7 @@ const Auth = require("@controllers/auth");
 
 router.get("/list/:page?", Auth.requireAdmin, Works.getList);
 
-router.delete("/:id", Auth.requireAdmin, Works.deleteWorkingById);
+router.delete("/:id([0-9]+)", Auth.requireAdmin, Works.deleteWorkingById);
+router.delete("/all", Auth.requireAdmin, Works.deleteWorkingAll);
 
 module.exports = router;
