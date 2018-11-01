4bb5d39d6e30c65ad9a7c7416e2085fe5e0d515a
Micalgenus <micalgenus@gmail.com>
2018-11-02 01:42:42 +0900

    Update database

--- models/shop.js
+++ models/shop.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
       _id: { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
       OWNERID: { type: DataTypes.INTEGER.UNSIGNED },
       OPEN: { type: DataTypes.BOOLEAN, defaultValue: false },
-      // DELIVERY: { type: DataTypes.BOOLEAN, defaultValue: false },
+      DELIVERY: { type: DataTypes.BOOLEAN, defaultValue: false },
       SHOPNAME: { type: DataTypes.STRING, notNull: true },
       PHONE: { type: DataTypes.STRING },
       HOMEPAGE: { type: DataTypes.STRING }
